4c6174471e87a365bf855b8fef664b97bdd19251
Minhyeok Choi <bbbim5656@gmail.com>
2026-09-20 22:46:21 +0900

    first commit

---
+++ src/main/java/io/github/minhyeok/coupon_prac/CouponPracApplication.java
@@ -0,0 +1,13 @@
+package io.github.minhyeok.coupon_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/minhyeok/coupon_prac/CouponPracApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.minhyeok.coupon_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
